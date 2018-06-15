bcf26759f9b0890ed6218f8ef7dedeb3da42a875
vianna274 <leonardo.vianna.f@gmail.com>
2018-06-15 03:25:47 -0300

    new illumination

--- src/shader_fragment.glsl
+++ src/shader_fragment.glsl
@@ -64,11 +64,14 @@ void main()
     vec4 n = normalize(normal);
 
     // Vetor que define o sentido da fonte de luz em relação ao ponto atual.
-    vec4 l = normalize(vec4(1.0,1.0,0.5,0.0));
-    l = normalize(camera_position - position_world);
+    vec4 l = normalize(camera_position - position_world);
+    vec4 l2 = normalize(vec4(-1,1,-1,0.0));
+
     // Vetor que define o sentido da câmera em relação ao ponto atual.
     vec4 v = normalize(camera_position - p);
+    // l = v;
     vec4 r =  -l + 2 * n * (dot(n, l));
+    vec4 r2 =  -l2 + 2 * n * (dot(n, l2));
 
     // Coordenadas de textura U e V
     float U = 0.0;
@@ -77,35 +80,8 @@ void main()
     vec3 Ks; // Refletância especular
     vec3 Ka; // Refletância ambiente
     float q;
-    if ( object_id == SPHERE )
-    {
-        // PREENCHA AQUI as coordenadas de textura da esfera, computadas com
-        // projeção esférica EM COORDENADAS DO MODELO. Utilize como referência
-        // o slide 139 do documento "Aula_20_e_21_Mapeamento_de_Texturas.pdf".
-        // A esfera que define a projeção deve estar centrada na posição
-        // "bbox_center" definida abaixo.
 
-        // Você deve utilizar:
-        //   função 'length( )' : comprimento Euclidiano de um vetor
-        //   função 'atan( , )' : arcotangente. Veja https://en.wikipedia.org/wiki/Atan2.
-        //   função 'asin( )'   : seno inverso.
-        //   constante M_PI
-        //   variável position_model
-        vec4 bbox_center = (bbox_min + bbox_max) / 2.0;
-        float raio = length(bbox_center - bbox_max);
-        vec4 p_ = bbox_center + raio* normalize(position_model - bbox_center);
-        vec4 pvector = p_ - bbox_center;
-
-        float px = pvector.x;
-        float py = pvector.y;
-        float pz = pvector.z;
-        float ro = sqrt(pow(px,2) + pow(py,2) + pow(pz,2));
-        float omega = atan(px,pz);
-        float delta = asin(py/ro);
-        U = (omega + M_PI)/(2*M_PI);
-        V = (delta + (M_PI_2))/(M_PI);
-    }
-    else if ( object_id == PLANE )
+    if ( object_id == PLANE )
     {
         // Coordenadas de textura do plano, obtidas do arquivo OBJ.
         U = texcoords.x;
@@ -113,7 +89,7 @@ void main()
         Kd = vec3(0.8,0.2,0.2);
         Ks = vec3(0.2,0.2,0.2);
         Ka = vec3(0.0,0.0,0.0);
-        q = 1.0;
+        q = 0.9;
     }
     else if ( object_id == WALL )
     {
@@ -123,7 +99,16 @@ void main()
         Kd = vec3(0.8,0.2,0.2);
         Ks = vec3(0.2,0.2,0.2);
         Ka = vec3(0.0,0.0,0.0);
-        q = 50.0;
+        q = 10;
+    }
+    else if ( object_id == BUNNY )
+    {
+        // PREENCHA AQUI
+        // Propriedades espectrais do coelho
+        Kd = vec3(0.08,0.4,0.8);
+        Ks = vec3(0.8,0.8,0.8);
+        Ka = vec3(0.04,0.2,0.4);
+        q = 32.0;
     }
     else if ( object_id == PISTOL )
     {
@@ -145,36 +130,38 @@ void main()
         Ka = vec3(0.0,0.0,0.0);
         q = 1.0;
     }
-    vec3 I = vec3(0.2,0.2,0.2);
-    // Obtemos a refletância difusa a partir da leitura da imagem TextureImage0
-    vec3 Ia = vec3(0.05,0.05,0.05); // PREENCHA AQUI o espectro da luz ambiente
+    vec3 I = vec3(0.6,0.6,0.6);
+    vec3 I2 = vec3(0.1,0.1,0.1);
+    vec3 Ia = vec3(0.3,0.3,0.3); // PREENCHA AQUI o espectro da luz ambiente
 
     vec3 Kd0 = texture(TextureImage2, vec2(U,V)).rgb;
     vec3 ambient_term = Ka * Ia;
     vec3 phong_specular_term  = Ks * I * pow(max(0, dot(r, v)), q);
+    vec3 phong_specular_term2  = Ks * I2 * pow(max(0, dot(r2, v)), q);
 
     if (object_id == PLANE) {
       Kd0 = texture(TextureImage0, vec2(U,V)).rgb;
+      // ambient_term = vec3(0,0,0);
+      // phong_specular_term2 = vec3(0,0,0);
     } else if (object_id == WALL) {
       Kd0 = texture(TextureImage1, vec2(U,V)).rgb;
       ambient_term = vec3(0,0,0);
       phong_specular_term = vec3(0,0,0);
     } else if (object_id == PISTOL) {
      Kd0 = texture(TextureImage2, vec2(U,V)).rgb;
-   } else if (object_id == SHOT) {
+    } else if (object_id == SHOT) {
       Kd0 = Kd;
     }
     vec3 lambert_diffuse_term = Kd0 * I * max(0, dot(n, l));
-
-    // Termo ambiente
-
-    // Termo especular utilizando o modelo de iluminação de Phong
+    vec3 lambert_diffuse_term2 = Kd0 * I2 * max(0, dot(n, l2));
 
     // Cor final do fragmento calculada com uma combinação dos termos difuso,
     // especular, e ambiente. Veja slide 134 do documento "Aula_17_e_18_Modelos_de_Iluminacao.pdf".
-    color = lambert_diffuse_term + ambient_term + phong_specular_term;
+    color =  phong_specular_term  + lambert_diffuse_term + ambient_term +
+             phong_specular_term2 + lambert_diffuse_term2;
 
-    // Cor final com correção gamma, considerando monitor sRGB.
-    // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
+    color = color/(2*(sqrt(pow(camera_position.x - position_world.x,2) +
+                pow(camera_position.y - position_world.y,2) +
+                pow(camera_position.z - position_world.z,2))));
     color = pow(color, vec3(1.0,1.0,1.0)/2.2);
 }
